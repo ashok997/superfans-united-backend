2a338285e67f1df96093dc80ec8eb5af311d5f75
ashok997 <ashok997@gmail.com>
2020-03-13 09:52:24 -0500

    added user controller at the backend

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: users, include: :user_characters
+    end
+
+    def create
+        user = User.create(user_params)
+        render json: user
+    end
+
+    def update
+        user = User.first
+        user.update(user_params)
+        render json: user
+    end
+
+    def show
+        user = User.first
+        render json: user, include: :user_characters
+    end
+
+
+    private
+    def character_params
+        params.require(:character).permit(:name,:email)
+    end
 end
