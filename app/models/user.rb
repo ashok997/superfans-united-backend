9e29a47fe824149712d701d9e97a06aac2ec6c5e
ashok997 <ashok997@gmail.com>
2020-08-17 14:06:51 -0500

    changed user model to include password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_many :user_characters
     has_many :characters, -> {distinct},  through: :user_characters
-    validates :email, presence:true
+   
+    has_secure_password
+
+    validates :email, :username, presence: true
+    validates :email, :username, uniqueness: true 
 end
