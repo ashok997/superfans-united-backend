6a15e66021e9745ca6be70b897e005d74dab5499
ashok997 <ashok997@gmail.com>
2020-08-18 15:09:01 -0500

    added routes for signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/signup' => 'api/v1/users#new'
+  post '/signup' => 'api/v1/users#create'
+
+
   namespace :api do
     namespace :v1 do
     resources :characters
