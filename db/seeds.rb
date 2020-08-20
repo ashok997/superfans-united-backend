7cf81d21251b749257b0a76adc7fbec6c2b73f31
ashok997 <ashok997@gmail.com>
2020-08-20 14:43:12 -0500

    dropped the database and seeded user with password

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-user1 =User.create(name:'Ashok Maharjan', email:'ashok@ashok.com')
+user1 =User.create(username:'am', email:'ashok@ashok.com', password: "pass")
 
-user2 =User.create(name:'Tony Stark', email:'tony@starkindustries.com')
+user2 =User.create(username:'tonystark', email:'tony@starkindustries.com', password: "secretword")
 
-user3 =User.create(name:'Bruce Banner', email:'bruceakahulk@avengers.com')
+user3 =User.create(username:'brucebanner', email:'bruceakahulk@avengers.com', password: "notsosecretword")
 
 character1 =Character.create(name:'Thor (Marvel Heroes)', description:"", thumbnail:'http://i.annihil.us/u/prod/marvel/i/mg/c/20/5239be0b8ecd1', image:'test')
 
 character2 =Character.create(name:'Loki', description:"", thumbnail:'http://i.annihil.us/u/prod/marvel/i/mg/d/90/526547f509313', image:'test2')
 
-usercharacter1 =UserCharacter.create(user_id: '1', comments: 'He is strange!', character_id: '2')
+usercharacter1 =UserCharacter.create(user_id: '1', comments: 'He is a son of Odin!', character_id: '2')
 
 usercharacter2 =UserCharacter.create(user_id: '1', comments: 'Random comment', character_id: '1')
 
