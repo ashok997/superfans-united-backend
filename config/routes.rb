07dbe82e59daa2a5e0d46b5ceb3de57451a5e13f
ashok997 <ashok997@gmail.com>
2020-09-26 13:57:39 -0500

    removed typo from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post '/signup' => 'api/v1/users#create'
 
   post '/api/v1/login' =>'api/v1/sessions#create'
-  post '/api/v1/logout' =>'api/v1/sessions#destroy'
+  delete '/api/v1/logout' =>'api/v1/sessions#destroy'
   get '/api/v1/get_current_user' => 'api/v1/sessions#get_current_user'
 
 
